aab2d27ae5a8c852a96fd22eb3f0f843aed4d2b6
daodao97 <daodao97@foxmail.com>
2025-12-29 23:34:55 +0800

    响应流中出现 error, 返回重试

--- backend/internal/service/gateway_service.go
+++ backend/internal/service/gateway_service.go
@@ -695,6 +695,11 @@ func (s *GatewayService) Forward(ctx context.Context, c *gin.Context, account *A
 	if req.Stream {
 		streamResult, err := s.handleStreamingResponse(ctx, resp, c, account, startTime, originalModel, req.Model)
 		if err != nil {
+			if err.Error() == "have error in stream" {
+				return nil, &UpstreamFailoverError{
+					StatusCode: 403,
+				}
+			}
 			return nil, err
 		}
 		usage = streamResult.usage
